71ad16279ecaacd32d73b5e9ecd261473a49ef55
YoussGm3o8 <youssefelkesti2005@gmail.com>
2024-12-03 13:53:40 -0500

    main2 return home TOFIX, try to follow red line/wall... Cannot detect when wall in front of robot, sensor opposite to wall gets interference. Try to deactivate one us sensor and check each at a time..

--- final_project/main2.py
+++ final_project/main2.py
@@ -1,13 +1,15 @@
 import subsystem.car as car
 import time
 from common.constants_params import *
-from common.filters import Diff
+from collections import deque
 
 
 
-TURN_SPEED = 150
-FORWARD_SPEED = 150
-TIMER = 150
+
+TURN_SPEED = 200
+FORWARD_SPEED = 200
+TIMER = 120
+YELLOW_CONFIDENCE_COUNT = 6
 
 car = car.Car(debug=False)
 
@@ -48,16 +50,28 @@ def check_cube(car):
             avoiding = car.avoid_wall(25)
             if avoiding[0]:
                 if avoiding[1] == "right":
-                    car.turn_left(TURN_SPEED, 10)
+                    car.turn_left(TURN_SPEED, 20)
+                    if car.is_water()[0]:
+                        car.reverse(FORWARD_SPEED, 10)
+                        car.wait_for_action()
                     car.wait_for_action()
                 elif avoiding[1] == "left":
-                    car.turn_right(TURN_SPEED, 10)
+                    car.turn_right(TURN_SPEED, 20)
+                    if car.is_water()[0]:
+                        car.reverse(FORWARD_SPEED, 10)
+                        car.wait_for_action()
                     car.wait_for_action()
                 elif avoiding[1] == "front":
-                    car.turn_right(TURN_SPEED, 20)
+                    car.turn_right(TURN_SPEED, 40)
+                    if car.is_water()[0]:
+                        car.reverse(FORWARD_SPEED, 10)
+                        car.wait_for_action()
                     car.wait_for_action()
             else:
                 car.turn_right(TURN_SPEED, 5)
+                if car.is_water()[0]:
+                        car.reverse(FORWARD_SPEED, 5)
+                        car.wait_for_action()
                 car.wait_for_action()
 
 
@@ -88,7 +102,7 @@ def scan(car, duration=10, treshold=25):
 
 def check_wall(car):
     car.stop()
-    avoiding = car.avoid_wall(15)
+    avoiding = car.avoid_wall(10)
     if avoiding[0]:
         print("wall detected", avoiding[1])
 
@@ -116,35 +130,334 @@ def check_wall(car):
     else:
         scan(car)
 
+def is_on_orange_line(color):
+    """
+    Determines if the color sensor is detecting the orange line
+    """
+    return color == 'o'
+
+
+def is_confident_yellow(color):
+    """
+    Ensures the color is confidently yellow, avoiding false positives from green
+    """
+    return color == 'y'
+
+# def follow_wall(car, wall_side='right'):
+#     """
+#     Follow a specific wall while scanning for cubes and avoiding water
+#     wall_side can be 'right' or 'left'
+#     """
+#     # Yellow detection tracking
+#     yellow_left_queue = deque(maxlen=YELLOW_CONFIDENCE_COUNT)
+#     yellow_right_queue = deque(maxlen=YELLOW_CONFIDENCE_COUNT)
+#     count = 0
+#     while True:        
+#         car.update(0.05)
+#         # Check for water and avoid
+#         avoid_water(car)
+        
+#         # Track yellow confidence
+#         count += 1
+#         yellow_left_queue.append(is_confident_yellow(car.state.left_color_sensor[-1]))
+#         yellow_right_queue.append(is_confident_yellow(car.state.right_color_sensor[-1]))
+
+#         print(car.state.left_color_sensor[-1], car.state.right_color_sensor[-1])
+#         time.sleep(0.1)
+
+#         # Check if we've found the home area with high confidence
+#         if (all(yellow_left_queue) and all(yellow_right_queue) and count >= YELLOW_CONFIDENCE_COUNT):
+#             car.stop()
+#             print("Confident home area detected!")
+#             if wall_side == 'right':
+#                 us_wall = car.state.us_sensor_2 
+#                 us_opp = car.state.us_sensor
+
+#                 while True:
+#                     car.update()
+#                     us_wall = car.state.us_sensor
+#                     us_opp = car.state.us_sensor_2
+#                     print(us_wall, us_opp)
+#                     if us_opp == 255:
+#                         break
+#                     if us_wall > 8:  # If the wall gets too far
+#                         car.turn_right(TURN_SPEED, 2)  # Adjust towards the wall
+#                         car.wait_for_action()
+#                         car.forward(FORWARD_SPEED)
+#                         time.sleep(0.05)
+#                     elif us_wall < 8:  # If too close to the wall
+#                         car.turn_left(TURN_SPEED, 2)  # Adjust away from the wall
+#                         car.wait_for_action()
+#                         car.forward(FORWARD_SPEED)
+#                         time.sleep(0.05)
+#                     else:
+#                         car.forward(FORWARD_SPEED)
+#                         time.sleep(0.1)
+                        
+#             else:
+#                 us_wall = car.state.us_sensor_2
+#                 us_opp = car.state.us_sensor
+
+#                 while True:
+#                     car.update()
+#                     us_wall = car.state.us_sensor_2
+#                     us_opp = car.state.us_sensor
+#                     print(us_wall, us_opp)
+#                     if us_opp == 255:
+#                         break
+#                     if us_wall > 8:  # If the wall gets too far
+#                         car.turn_left(TURN_SPEED, 2)  # Adjust towards the wall
+#                         car.wait_for_action()
+#                         car.forward(FORWARD_SPEED)
+#                     elif us_wall < 8:  # If too close to the wall
+#                         car.turn_right(TURN_SPEED, 2)  # Adjust away from the wall
+#                         car.wait_for_action()
+#                         car.forward(FORWARD_SPEED)
+#                     else:
+#                         car.forward(FORWARD_SPEED)
+#                         time.sleep(0.1)
+
+
+        #     return
+        # us1, us2 = car.state.us_sensor, car.state.us_sensor_2
+        # # Wall following logic
+        # if wall_side == 'right':
+        #     if us1 > 15:  # If the wall gets too far
+        #         car.turn_right(TURN_SPEED, 2)  # Adjust towards the wall
+        #         car.wait_for_action()
+        #         car.forward(FORWARD_SPEED)
+        #     elif us1 < 5:  # If too close to the wall
+        #         car.turn_left(TURN_SPEED, 2)  # Adjust away from the wall
+        #         car.wait_for_action()
+        #         car.forward(FORWARD_SPEED)
+        #     else:
+        #         car.forward(FORWARD_SPEED)
+        #         time.sleep(0.1)
+        # else:  # left wall following
+        #     if us2 > 15:  # If the wall gets too far
+        #         car.turn_left(TURN_SPEED, 2)  # Adjust towards the wall
+        #         car.wait_for_action()
+        #         car.forward(FORWARD_SPEED)
+        #     elif us2 < 5:  # If too close to the wall
+        #         car.turn_right(TURN_SPEED, 2)  # Adjust away from the wall
+        #         car.wait_for_action()
+        #         car.forward(FORWARD_SPEED)
+        #     else:
+        #         car.forward(FORWARD_SPEED)
+        #         time.sleep(0.1)
+
+def follow_wall_with_line(car, wall_side='right'):
+    """
+    Follow a specific wall while tracking the orange line and scanning for cubes and avoiding water
+    wall_side can be 'right' or 'left'
+    """
+    # Orange line detection tracking
+    orange_left_queue = deque(maxlen=1)
+    orange_right_queue = deque(maxlen=1)
+    
+    # Yellow confidence tracking for home area detection
+    yellow_left_queue = deque(maxlen=YELLOW_CONFIDENCE_COUNT)
+    yellow_right_queue = deque(maxlen=YELLOW_CONFIDENCE_COUNT)
+    
+    count = 0
+    while True:        
+        car.update(0.05)
+        
+        # Check for water and avoid
+        avoid_water(car)
+        
+        # Track orange line confidence
+        orange_left_queue.append(is_on_orange_line(car.state.left_color_sensor[-1]))
+        orange_right_queue.append(is_on_orange_line(car.state.right_color_sensor[-1]))
+        
+        # Track yellow line confidence for home area
+        yellow_left_queue.append(is_confident_yellow(car.state.left_color_sensor[-1]))
+        yellow_right_queue.append(is_confident_yellow(car.state.right_color_sensor[-1]))
+        
+        count += 1
+        
+        # Line following and wall following logic
+        if wall_side == 'right':
+            us1 = car.state.us_sensor
+            if us1 > 15:  # If the wall gets too far
+                car.turn_right(TURN_SPEED, 2)  # Adjust towards the wall
+                car.wait_for_action()
+                car.forward(FORWARD_SPEED)
+            elif us1 < 5:  # If too close to the wall
+                car.turn_left(TURN_SPEED, 2)  # Adjust away from the wall
+                car.wait_for_action()
+                car.forward(FORWARD_SPEED)
+            else:
+                # Fine-tune line following based on color sensors
+                if all(orange_left_queue) and not all(orange_right_queue):
+                    # Drift right to center on line
+                    car.turn_right(TURN_SPEED, 1)
+                elif all(orange_right_queue) and not all(orange_left_queue):
+                    # Drift left to center on line
+                    car.turn_left(TURN_SPEED, 1)
+                else:
+                    car.forward(FORWARD_SPEED)
+                    time.sleep(0.1)
+        else:  # left wall following
+            us2 = car.state.us_sensor_2
+            if us2 > 15:  # If the wall gets too far
+                car.turn_left(TURN_SPEED, 2)  # Adjust towards the wall
+                car.wait_for_action()
+                car.forward(FORWARD_SPEED)
+            elif us2 < 5:  # If too close to the wall
+                car.turn_right(TURN_SPEED, 2)  # Adjust away from the wall
+                car.wait_for_action()
+                car.forward(FORWARD_SPEED)
+            else:
+                # Fine-tune line following based on color sensors
+                if all(orange_left_queue) and not all(orange_right_queue):
+                    # Drift right to center on line
+                    car.turn_right(TURN_SPEED, 1)
+                elif all(orange_right_queue) and not all(orange_left_queue):
+                    # Drift left to center on line
+                    car.turn_left(TURN_SPEED, 1)
+                else:
+                    car.forward(FORWARD_SPEED)
+                    time.sleep(0.1)
+        
+        # Check if we've found the home area with high confidence
+        if (all(yellow_left_queue) and all(yellow_right_queue) and count >= YELLOW_CONFIDENCE_COUNT):
+            car.stop()
+            print("Confident home area detected!")
+            
+            # Precise wall-following in home area
+            if wall_side == 'right':
+                us_wall = car.state.us_sensor
+                us_opp = car.state.us_sensor_2
+
+                while True:
+                    car.update()
+                    us_wall = car.state.us_sensor
+                    us_opp = car.state.us_sensor_2
+                    print(us_wall, us_opp)
+                    
+                    if us_opp == 255:
+                        break
+                    
+                    if us_wall > 8:  # If the wall gets too far
+                        car.turn_right(TURN_SPEED, 2)  # Adjust towards the wall
+                        car.wait_for_action()
+                        car.forward(FORWARD_SPEED)
+                        time.sleep(0.05)
+                    elif us_wall < 8:  # If too close to the wall
+                        car.turn_left(TURN_SPEED, 2)  # Adjust away from the wall
+                        car.wait_for_action()
+                        car.forward(FORWARD_SPEED)
+                        time.sleep(0.05)
+                    else:
+                        car.forward(FORWARD_SPEED)
+                        time.sleep(0.1)
+                        
+            else:
+                us_wall = car.state.us_sensor_2
+                us_opp = car.state.us_sensor
+
+                while True:
+                    car.update()
+                    us_wall = car.state.us_sensor_2
+                    us_opp = car.state.us_sensor
+                    print(us_wall, us_opp)
+                    
+                    if us_opp == 255:
+                        break
+                    
+                    if us_wall > 8:  # If the wall gets too far
+                        car.turn_left(TURN_SPEED, 2)  # Adjust towards the wall
+                        car.wait_for_action()
+                        car.forward(FORWARD_SPEED)
+                    elif us_wall < 8:  # If too close to the wall
+                        car.turn_right(TURN_SPEED, 2)  # Adjust away from the wall
+                        car.wait_for_action()
+                        car.forward(FORWARD_SPEED)
+                    else:
+                        car.forward(FORWARD_SPEED)
+                        time.sleep(0.1)
+
+            return
+        
 def return_home(car):
     """
-    assume you are at a border facing the wall
+    Smart return home function that:
+    1. Determines which wall is closer to follow
+    2. Follows that wall while scanning for cubes, avoiding water, and tracking the orange line
+    3. Stops and dumps cubes when home area is detected with high confidence
     """
-    car.forward(FORWARD_SPEED)
-    while True:
-        car.update(0.05)
-        if car.avoid_wall(15):
-            break
-        avoid_water(car)
+    # Determine wall distances
+    right_wall_dist = car.state.us_sensor
+    left_wall_dist = car.state.us_sensor_2
+    
+    print(f"Wall distances - Right: {right_wall_dist}, Left: {left_wall_dist}")
+    
+    # Follow the closer wall with line tracking
+    if right_wall_dist < left_wall_dist:
+        print("Following right wall with line tracking")
+        follow_wall_with_line(car, wall_side='right')
+    else:
+        print("Following left wall with line tracking")
+        follow_wall_with_line(car, wall_side='left')
+    
+    # Once in home area, dump cubes
+    print("Home area reached!")
 
-    car.turn_left(TURN_SPEED)
-    while(car.state.right_color_sensor[-1] != 'o'):
-        print(car.state.right_color_sensor) #this may be white
-        car.update(0.05)
+# def return_home(car):
+#     """
+#     assume you are at a border facing the wall
+#     """
+#     count = 0
+#     while count < 5:
+#         car.update(0.05)
+#         if (car.state.right_color_sensor[-1] != 'y' and car.state.left_color_sensor[-1] != 'y'):
+#             print(car.state.right_color_sensor[-1], car.state.left_color_sensor[-1])
+#             if car.avoid_wall(10) == (True, "right"):
+#                 car.turn_left(TURN_SPEED, 5)
+#                 car.wait_for_action()
+#                 car.forward(FORWARD_SPEED)
+#             else:
+#                 car.turn_right(TURN_SPEED, 5)
+#         elif (car.state.right_color_sensor[-1] == 'y' and car.state.left_color_sensor[-1] == 'y'):
+#             count += 1
+#             print("was yellow at count:", count)
+#             print(car.state.right_color_sensor[-1], car.state.left_color_sensor[-1])
+#         else:
+#             count = 0
+    # car.forward(FORWARD_SPEED)
+    # while True:
+    #     car.update(0.05)
+    #     if car.avoid_wall(15):
+    #         break
+    #     avoid_water(car)
 
-    car.turn_right(TURN_SPEED)
-    while(car.state.right_color_sensor[-1] == 'o'):
-        car.update(0.05)
+    # car.turn_left(TURN_SPEED)
+    # while(car.state.right_color_sensor[-1] != 'o'):
+    #     print(car.state.right_color_sensor) #this may be white
+    #     car.update(0.05)
+    # car.stop()
 
-    while True:
-        car.update(0.05)
-        if car.state.right_color_sensor[-1] == 'o':
-            time.sleep(0.1) #adjust to overshoot
-            car.wheel_left(TURN_SPEED) #if this makes no sense its because the ports are wrong so ignore
-            car.wheel_right(150)
-        else:
-            car.wheel_left(150)
-            car.wheel_right(TURN_SPEED)
+    # car.turn_right(TURN_SPEED)
+    # while(car.state.right_color_sensor[-1] == 'o'):
+    #     car.update(0.05)
+    # car.stop()
+
+    # while True:
+    #     car.update(0.05)
+    #     print(car.state.right_color_sensor[-1], car.state.left_color_sensor[-1])
+    #     if car.state.right_color_sensor[-1] == 'o':
+    #         time.sleep(0.1) #adjust to overshoot
+    #         car.turn_left(TURN_SPEED/2) #if this makes no sense its because the ports are wrong so ignore
+    #         #car.turn_right(150)
+    #     else:
+    #         #car.turn_left(150)
+    #         car.turn_right(TURN_SPEED/2)
+        
+    #     if car.state.right_color_sensor[-1] == 'y' and car.state.left_color_sensor[-1] == 'y':
+    #         print(car.state.right_color_sensor[-1], car.state.left_color_sensor[-1])
+    #         car.stop()
+    #         car.dump_cubes()
 
 
 try:
@@ -160,26 +473,38 @@ try:
         avoid_water(car)
         scan(car)
 
-    while car.avoid_wall(100)[1] != "front":
-        car.update()
-        time.sleep(0.05)
-        print("not front")
-        if car.avoid_wall(100)[1] == "right":
-            print("right")
-            car.turn_right(100, 10)
-            car.wait_for_action()
-            car.forward(100)
-            time.sleep(0.5)
-            car.stop() 
-        if car.avoid_wall(100)[1] == "left":
-            print("left")
-            car.turn_left(100, 10)
-            car.wait_for_action()
-            car.forward(100)
-            time.sleep(0.5)
-            car.stop() 
+    # while car.avoid_wall(10)[1] != "right":
+    #     time.sleep(0.05)
+    #     car.forward(FORWARD_SPEED)
+    #     car.scan()
+    #     print("not right")
+
+        # if car.avoid_wall(40)[1] == "front":
+        #     car.reverse(FORWARD_SPEED, 10)
+        #     car.turn_left(TURN_SPEED, 20)
+        # if car.avoid_wall(20)[1] == "right":
+        #     print("right")
+        #     car.turn_right(TURN_SPEED, 10)
+        #     car.wait_for_action()
+        #     car.forward(FORWARD_SPEED)
+        #     time.sleep(0.5)
+        #     car.stop() 
+        # if car.avoid_wall(40)[1] == "left":
+        #     print("left")
+        #     car.turn_right(TURN_SPEED, 60)
+        #     car.wait_for_action()
+        #     car.forward(FORWARD_SPEED)
+        #     time.sleep(0.5)
+        #     car.stop()
+        # else:
+        #     car.forward(FORWARD_SPEED)
+        #     car.avoid_water()
+        #     car.scan()
+    # print("wall found, returning home")
     return_home(car)
     car.dump_cubes()
+    time.sleep(1)
+
 finally:
     car.kill()
 
